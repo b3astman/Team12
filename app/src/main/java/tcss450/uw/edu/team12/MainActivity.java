206295276e5f77491b84fd4bedb3c02a1b87ddf1
Bethany Eastman <bethanyeastman@gmail.com>
2016-05-02 15:23:53 -0700

    added logout, and menu for favorites.

--- app/src/main/java/tcss450/uw/edu/team12/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/team12/MainActivity.java
@@ -1,10 +1,19 @@
 package tcss450.uw.edu.team12;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import tcss450.uw.edu.team12.authenticate.SignInActivity;
 import tcss450.uw.edu.team12.model.Route;
 import tcss450.uw.edu.team12.model.Stop;
 
@@ -21,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements BusStopsListFragm
         //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
 
+
         if (savedInstanceState == null || getSupportFragmentManager().findFragmentById(R.id.list) == null) {
             BusStopsListFragment stopsListFragment = new BusStopsListFragment();
             getSupportFragmentManager().beginTransaction()
@@ -30,6 +40,34 @@ public class MainActivity extends AppCompatActivity implements BusStopsListFragm
         }
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater findMenuItems = getMenuInflater();
+        findMenuItems.inflate(R.menu.menu_bus, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                    SharedPreferences sharedPreferences =
+                            getSharedPreferences(getString(R.string.LOGIN_PREFS), Context.MODE_PRIVATE);
+                    sharedPreferences.edit().putBoolean(getString(R.string.LOGGEDIN), false)
+                            .commit();
+
+                    Intent i = new Intent(this, SignInActivity.class);
+                    startActivity(i);
+                    finish();
+                Toast.makeText(MainActivity.this, "You have logged out logout", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_favorites:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 //    @Override
 //    public void onListFragmentInteraction(Stop stop) {
 //        StopDetailFragment stopDetailFragment = new StopDetailFragment();
@@ -42,6 +80,14 @@ public class MainActivity extends AppCompatActivity implements BusStopsListFragm
 //                .addToBackStack(null)
 //                .commit();
 //
+//    }
+
+//    @Override
+//    public void onCreateContextMenu(ContextMenu menu, View v,
+//                                    ContextMenu.ContextMenuInfo menuInfo) {
+//        super.onCreateContextMenu(menu, v, menuInfo);
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu_bus, menu);
 //    }
 
     @Override
