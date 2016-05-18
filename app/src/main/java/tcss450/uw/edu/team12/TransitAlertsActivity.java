a21f24fcfbf34917d97da17814fa7b7b1ce0df18
Lachezar <ldimov@uw.edu>
2016-05-17 20:22:59 -0700

    Transit alerts feature implemented.
    User can see a list of transit alerts for King County Metro that comes from an RSS feed. The list contains a title, as well as a
    link that can be followed in order to get detailed information on the selected alert.
    
    'View favorite stops' feature implemented.
    
    Favorite stops can be viewed by tapping on the 'Favorite stops' menu item. The user can also remove a given stop from their
    list of favorites by tapping on the three dots (next to the stop id). The RecyclerViewAdapter gets notified of the dataset changes
    that were made, so once the stop has been deleted, the change gets reflected instantly.
    
    'Add to favorites' feature implemented.
    
    If stop does not exist in the db file (favorite stops) on the device, then it gets added. Otherwise, the user gets notified that
    the stop already exists.
    
    Added context menu for each bus stop recycler view item for the 'add to favorites' functionality.
    
    The "three-dot" overflow pattern was used as icon. Adding a bus stop to favorites needs to be programmed.

---
+++ app/src/main/java/tcss450/uw/edu/team12/TransitAlertsActivity.java
@@ -0,0 +1,131 @@
+package tcss450.uw.edu.team12;
+
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import java.net.URL;
+
+public class TransitAlertsActivity extends AppCompatActivity {
+
+
+//    public static final String WIFI = "Wi-Fi";
+    public static final String ANY = "Any";
+    private static final String URL = "https://public.govdelivery.com/topics/WAKCDOT_255/feed.rss";
+
+    public static String sPref = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transit_alerts);
+
+        setTitle(getResources().getText(R.string.transit_alerts));
+        loadPage();
+    }
+
+    // Uses AsyncTask to download the XML feed from King County Metro (feed hosted on another domain).
+    public void loadPage() {
+
+        new DownloadXmlTask().execute(URL);
+    }
+
+    // Implementation of AsyncTask used to download XML feed from King County Metro
+    private class DownloadXmlTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadXmlFromNetwork(urls[0]);
+            } catch (IOException e) {
+                return getResources().getString(R.string.connection_error);
+            } catch (XmlPullParserException e) {
+                return getResources().getString(R.string.xml_error);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+//            setContentView(this);
+            // Displays the HTML string in the UI via a WebView
+            WebView myWebView = (WebView) findViewById(R.id.webView);
+            myWebView.loadData(result, "text/html", null);
+
+
+        }
+    }
+
+    // Uploads XML, parses it, and combines it with
+// HTML markup. Returns HTML string.
+    private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+        InputStream stream = null;
+        // Instantiate the parser
+        TransitAlertsXmlParser transitAlertsXmlParser = new TransitAlertsXmlParser();
+        List<TransitAlertsXmlParser.Item> items = null;
+        String title = null;
+        String url = null;
+//        String summary = null;
+//        Calendar rightNow = Calendar.getInstance();
+//        DateFormat formatter = new SimpleDateFormat("MMM dd h:mmaa");
+
+        // Checks whether the user set the preference to include summary text
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean pref = sharedPrefs.getBoolean("summaryPref", false);
+
+        StringBuilder htmlString = new StringBuilder();
+        // Append current time
+        // htmlString.append("<em>" + formatter.format(rightNow.getTime()) + "</em>");
+
+        try {
+            stream = downloadUrl(urlString);
+            items = transitAlertsXmlParser.parse(stream);
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+
+        // TransitAlertsXmlParser returns a List (called "items") of Item objects.
+        // Each Item object represents a single post in the XML feed.
+        // This section processes the items list to combine each entry with HTML markup.
+        // Each item is displayed in the UI as a link
+        for (TransitAlertsXmlParser.Item item : items) {
+            htmlString.append(item.pubDate);
+            htmlString.append("<p><a href='");
+            htmlString.append(item.link);
+            htmlString.append("'>" + item.title + "</a></p>");
+
+        }
+        return htmlString.toString();
+    }
+
+    // Given a string representation of a URL, sets up a connection and gets
+    // an input stream.
+    private InputStream downloadUrl(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Starts the query
+        conn.connect();
+        return conn.getInputStream();
+    }
+
+}
